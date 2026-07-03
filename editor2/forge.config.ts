44db3d0c1bdd1c4ed9189aabd919158f72cb9c9e
Richard Thorell <richard.thorell@outlook.com>
2026-07-02 22:05:55 -0700

    Add Electron Forge config

---
+++ editor2/forge.config.ts
@@ -0,0 +1,46 @@
+import type { ForgeConfig } from '@electron-forge/shared-types';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
+import { VitePlugin } from '@electron-forge/plugin-vite';
+
+const config: ForgeConfig = {
+  packagerConfig: {
+    asar: true,
+    executableName: 'arc-editor2',
+  },
+  rebuildConfig: {},
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ['darwin']),
+    new MakerDeb({}),
+    new MakerRpm({}),
+  ],
+  plugins: [
+    new AutoUnpackNativesPlugin({}),
+    new VitePlugin({
+      build: [
+        {
+          entry: 'src/main/main.ts',
+          config: 'vite.main.config.ts',
+          target: 'main',
+        },
+        {
+          entry: 'src/preload/preload.ts',
+          config: 'vite.preload.config.ts',
+          target: 'preload',
+        },
+      ],
+      renderer: [
+        {
+          name: 'main_window',
+          config: 'vite.renderer.config.ts',
+        },
+      ],
+    }),
+  ],
+};
+
+export default config;
