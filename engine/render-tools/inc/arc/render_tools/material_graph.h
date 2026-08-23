2527aa9dda7819d06771ea006cf4eee85e9823a0
Richard Thorell <richard.thorell@outlook.com>
2026-08-22 22:47:23 -0700

    render-tools: expose advanced Material ABI graph outputs

--- engine/render-tools/inc/arc/render_tools/material_graph.h
+++ engine/render-tools/inc/arc/render_tools/material_graph.h
@@ -145,7 +145,22 @@ enum class material_surface_output : std::uint8_t
     ambient_occlusion,
     emissive,
     opacity,
-    alpha_cutoff
+    alpha_cutoff,
+    clear_coat,
+    clear_coat_roughness,
+    clear_coat_normal,
+    sheen,
+    sheen_color,
+    sheen_roughness,
+    anisotropy,
+    anisotropy_rotation,
+    transmission,
+    index_of_refraction,
+    thickness,
+    attenuation_color,
+    attenuation_distance,
+    subsurface_color,
+    subsurface
 };
 
 /** @brief Connection selected for one material surface output. */
