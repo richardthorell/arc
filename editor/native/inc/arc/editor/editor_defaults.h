a265f265569ea4a895f255da8867d3c0efc743d8
Richard Thorell <richard.thorell@outlook.com>
2026-09-04 11:37:42 -0700

    Adjust default camera to closer third-person framing

--- editor/native/inc/arc/editor/editor_defaults.h
+++ editor/native/inc/arc/editor/editor_defaults.h
@@ -14,11 +14,11 @@ inline constexpr math::vector3f fallback_mesh_bounds_max{0.5f, 0.5f, 0.5f};
 inline constexpr float fallback_mesh_radius = 1.0f;
 inline constexpr float imported_mesh_fit_size = 1.8f;
 
-inline constexpr math::vector3f default_camera_position{8.0f, 6.0f, 8.0f};
-inline constexpr math::vector3f default_camera_focus{0.0f, 0.0f, 0.0f};
-inline constexpr float default_camera_focus_radius = 8.0f;
-inline constexpr float default_camera_orbit_x = -45.0f;
-inline constexpr float default_camera_orbit_y = 32.0f;
+inline constexpr math::vector3f default_camera_position{4.5f, 3.0f, 5.5f};
+inline constexpr math::vector3f default_camera_focus{0.0f, 1.0f, 0.0f};
+inline constexpr float default_camera_focus_radius = 5.0f;
+inline constexpr float default_camera_orbit_x = -40.0f;
+inline constexpr float default_camera_orbit_y = 22.0f;
 inline constexpr float default_camera_near_plane = 0.25f;
 inline constexpr float default_camera_far_plane = 500.0f;
 
